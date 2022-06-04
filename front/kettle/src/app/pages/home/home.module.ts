cc2566bbc163b07397bf057b18b70cbe57c340ae
Maxime GENEVIER <maxime.genevier.sio@gmail.com>
2022-06-04 17:23:43 +0200

    [ADD] Finish front initialization

--- front/kettle/src/app/pages/home/home.module.ts
+++ front/kettle/src/app/pages/home/home.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-
-
 @NgModule({
   declarations: [
     HomeComponent
